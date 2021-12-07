47229939da8e84a9871048cda574eaf14a4d0d55
Thiago Brasil <tchaguitos@gmail.com>
2021-12-06 21:43:38 -0300

    feat(glicemias): add auditoria e ajusta testes

--- contextos/glicemias/dominio/entidades/glicemias.py
+++ contextos/glicemias/dominio/entidades/glicemias.py
@@ -1,5 +1,17 @@
-from datetime import datetime
+from typing import Optional
+from uuid import UUID, uuid4
+from datetime import date, datetime
 from dataclasses import dataclass
+
+
+@dataclass
+class Auditoria:
+    criado_por: UUID  # TODO: criar classe para usuario
+    data_criacao: datetime
+    ultima_vez_editado_por: Optional[UUID]
+    data_ultima_edicao: Optional[datetime]
+    ativo: bool = True
+    deletado: bool = False
 
 
 @dataclass
@@ -8,6 +20,7 @@ class Glicemia:
     jejum: bool
     data: datetime
     observacoes: str
+    auditoria: Auditoria
 
     @classmethod
     def criar_nova(
@@ -16,10 +29,19 @@ class Glicemia:
         jejum: bool,
         data: datetime,
         observacoes: str,
+        criado_por: UUID,
     ):
         return cls(
             valor=valor,
             jejum=jejum,
             data=data,
             observacoes=observacoes,
+            auditoria=Auditoria(
+                criado_por=criado_por,
+                data_criacao=datetime.now(),
+                ultima_vez_editado_por=None,
+                data_ultima_edicao=None,
+                ativo=True,
+                deletado=False,
+            ),
         )
--- tests/contextos/glicemias/dominio/test_entidades.py
+++ tests/contextos/glicemias/dominio/test_entidades.py
@@ -1,25 +1,41 @@
+from uuid import uuid4
 from datetime import datetime
 from freezegun import freeze_time
 
 from contextos.glicemias.dominio.entidades.glicemias import (
     Glicemia,
+    Auditoria,
 )
 
 
 @freeze_time(datetime(2021, 8, 27, 16, 20))
 def test_criar_nova_glicemia():
+    id_usuario = uuid4()
+
+    hora_atual = datetime.now()
+    hora_glicemia = datetime(2021, 8, 27, 8, 15)
+
     glicemia_esperada = Glicemia(
         valor=120,
         jejum=True,
-        data=datetime.now(),
+        data=hora_glicemia,
         observacoes="primeira glicemia do dia",
+        auditoria=Auditoria(
+            criado_por=id_usuario,
+            data_criacao=hora_atual,
+            ultima_vez_editado_por=None,
+            data_ultima_edicao=None,
+            ativo=True,
+            deletado=False,
+        ),
     )
 
     glicemia_criada = Glicemia.criar_nova(
         valor=120,
         jejum=True,
-        data=datetime.now(),
+        data=hora_glicemia,
         observacoes="primeira glicemia do dia",
+        criado_por=id_usuario,
     )
 
     assert glicemia_criada == glicemia_esperada

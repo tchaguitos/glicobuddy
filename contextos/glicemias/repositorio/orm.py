ce4385384b9c30537f3e7baf777390922d4ebdac
Thiago Brasil <tchaguitos@gmail.com>
2021-12-11 22:14:07 -0300

    feat(glicemias/repositorio): adiciona mappers para tabelas auditoria e glicemia

--- contextos/glicemias/repositorio/orm.py
+++ contextos/glicemias/repositorio/orm.py
@@ -2,7 +2,16 @@ from uuid import uuid4
 from datetime import datetime
 from sqlalchemy.orm import mapper, relationship
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Table, MetaData, Column, Boolean, String, Integer, DateTime, ForeignKey
+from sqlalchemy import (
+    Table,
+    MetaData,
+    Column,
+    Boolean,
+    String,
+    Integer,
+    DateTime,
+    ForeignKey,
+)
 
 from contextos.glicemias.dominio.entidades import Auditoria, Glicemia
 
@@ -20,13 +29,26 @@ tabela_auditoria = Table(
     Column("deletado", Boolean, default=False),
 )
 
-tabela_glicemias = Table(
+tabela_glicemia = Table(
     "glicemia",
     metadata,
     Column("id", UUID(as_uuid=True), primary_key=True, default=uuid4),
-    Column("valor", Integer)
-    Column("jejum", Boolean, default=False),
-    Column("data")
-    Column("observacoes")
-    # auditoria: Auditoria
+    Column("valor", Integer),
+    Column("observacoes", String(255)),
+    Column("primeira_do_dia", Boolean, default=False),
+    Column("horario_dosagem", DateTime),
+)
+
+
+mapper_auditoria = mapper(Auditoria, tabela_auditoria)
+
+mapper_glicemia = mapper(
+    Glicemia,
+    tabela_glicemia,
+    properties={
+        "auditoria": relationship(
+            mapper_auditoria,
+            secondary=tabela_auditoria,
+        )
+    },
 )

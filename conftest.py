6fcc7a64ead34fadb629be5bf66cf3be177e55f9
Thiago Brasil <tchaguitos@gmail.com>
2021-12-16 20:21:38 -0300

    test: ajusta fixtures para trabalhar com postgresql e inicia mappers

--- conftest.py
+++ conftest.py
@@ -3,18 +3,18 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, clear_mappers
 
-# from orm import metadata, start_mappers
+from contextos.glicemias.repositorio.orm import metadata, start_mappers
 
 
-@pytest.fixture
-def in_memory_db():
-    engine = create_engine("sqlite:///:memory:")
-    # metadata.create_all(engine)
+@pytest.fixture(scope="session")
+def conexao_db():
+    engine = create_engine("postgresql+psycopg2://glico:test@localhost/glicotest")
+    metadata.create_all(engine)
     return engine
 
 
-@pytest.fixture
-def session(in_memory_db):
-    # start_mappers()
-    yield sessionmaker(bind=in_memory_db)()
+@pytest.fixture(scope="session")
+def session(conexao_db):
+    start_mappers()
+    yield sessionmaker(bind=conexao_db)()
     clear_mappers()

32a913e4d0ea233171c4bbfadf87431ae3919f14
Thiago Brasil <tchaguitos@gmail.com>
2021-12-16 21:16:23 -0300

    test(conftest): add drop_all em fixture conexao_db

--- conftest.py
+++ conftest.py
@@ -1,20 +1,22 @@
 import pytest
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, clear_mappers
+from sqlalchemy.orm import sessionmaker
 
 from contextos.glicemias.repositorio.orm import metadata, start_mappers
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def conexao_db():
     engine = create_engine("postgresql+psycopg2://glico:test@localhost/glicotest")
+
+    metadata.drop_all(engine)
     metadata.create_all(engine)
+
     return engine
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def session(conexao_db):
     start_mappers()
     yield sessionmaker(bind=conexao_db)()
-    clear_mappers()

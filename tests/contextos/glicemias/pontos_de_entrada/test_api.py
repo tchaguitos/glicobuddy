335bcf56bd7d8445f4835f0a829b8c35a227e052
Thiago Brasil <tchaguitos@gmail.com>
2021-12-28 20:04:18 -0300

    test(glicemias/pontos_de_entrada): add teste para endpoint para cadastrar glicemias

---
+++ tests/contextos/glicemias/pontos_de_entrada/test_api.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from freezegun import freeze_time
+from fastapi.testclient import TestClient
+
+from contextos.glicemias.pontos_de_entrada.api import app
+
+from contextos.glicemias.dominio.entidades import Glicemia
+
+client = TestClient(app)
+
+
+@freeze_time(datetime(2021, 8, 27, 16, 20))
+def test_criar_glicemia():
+    horario_dosagem = datetime.now()
+
+    glicemia_a_ser_criada = {
+        "valor": 108,
+        "observacoes": "poxa ein teste de integracao nao",
+        "primeira_do_dia": True,
+        "horario_dosagem": str(horario_dosagem),
+    }
+    
+    response = client.post(
+        "/v1/glicemias",
+        json=glicemia_a_ser_criada,
+    )
+
+    assert "id" in response.json()
+    assert response.status_code == 201

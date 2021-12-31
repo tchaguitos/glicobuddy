e38f72e26d27f58cb675a6174ad3f5f3467aaba3
Thiago Brasil <tchaguitos@gmail.com>
2021-12-30 21:12:51 -0300

    feat(glicemias/servicos): add unidade de trabalho

---
+++ contextos/glicemias/servicos/unidade_de_trabalho.py
@@ -0,0 +1,57 @@
+import abc
+import config
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from contextos.glicemias.repositorio.repo_dominio import (
+    AbstractRepository,
+    SqlAlchemyRepository,
+)
+
+
+class AbstractUnitOfWork(abc.ABC):
+    repo: AbstractRepository
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @abc.abstractmethod
+    def commit(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def rollback(self):
+        raise NotImplementedError
+
+
+DEFAULT_SESSION_FACTORY = sessionmaker(
+    bind=create_engine(
+        config.get_postgres_uri(),
+    )
+)
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
+        self.session_factory = session_factory
+
+    def __enter__(self):
+        self.session: Session = self.session_factory()  # type: Session
+        self.repo: SqlAlchemyRepository = SqlAlchemyRepository(self.session)
+
+        return super().__enter__()
+
+    def __exit__(self, *args):
+        super().__exit__(*args)
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()

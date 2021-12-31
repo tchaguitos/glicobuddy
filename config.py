63ed6d6b7d59decd9e2278b1b342d88ea5cd5391
Thiago Brasil <tchaguitos@gmail.com>
2021-12-30 21:12:17 -0300

    config: add funcao para retornar uri do postgres

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from contextos.glicemias.repositorio.orm import metadata
 
 
-def get_session(engine = None, is_test: bool = False):
+def get_session(engine=None, is_test: bool = False):
     """"""
     if not engine:
         engine = create_engine("postgresql+psycopg2://glico:test@localhost/glicotest")
@@ -13,3 +15,11 @@ def get_session(engine = None, is_test: bool = False):
         metadata.create_all(engine)
 
     return sessionmaker(bind=engine)()
+
+
+def get_postgres_uri():
+    usuario = os.environ.get("DB_USER", "glico")
+    senha = os.environ.get("DB_PASSWORD", "test")
+    host = os.environ.get("DB_HOST", "localhost")
+    db_name = os.environ.get("DB_NAME", "glicotest")
+    return f"postgresql+psycopg2:://{usuario}:{senha}@{host}/{db_name}"

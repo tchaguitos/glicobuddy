4acbcfba7c2953ac571a10bee9d580d629589a6a
Thiago Brasil <tchaguitos@gmail.com>
2022-01-08 16:51:23 -0300

    feat(glicemias/servicos): add visualizadores para consultar todas as glicemias e consultar glicemias por id

--- contextos/glicemias/servicos/visualizadores.py
+++ contextos/glicemias/servicos/visualizadores.py
@@ -1,9 +1,31 @@
-from typing import List
+from uuid import UUID
+from typing import Iterator
 
 from contextos.glicemias.dominio.entidades import Glicemia
-from contextos.glicemias.repositorio.repo_dominio import AbstractRepository
+
+from contextos.glicemias.servicos.unidade_de_trabalho import AbstractUnitOfWork
 
 
-def consultar_glicemias(repo: AbstractRepository, sessao) -> List[Glicemia]:
-    glicemias = repo.consultar_todos()
-    yield from glicemias
+def consultar_glicemias(
+    uow: AbstractUnitOfWork,
+    usuario_id: UUID,
+) -> Iterator[Glicemia]:
+
+    with uow:
+        print(usuario_id)  # TODO: vincular glicemias a usuarios ou outra entidade
+        glicemias = uow.repo.consultar_todos()
+
+    return glicemias
+
+
+def consultar_glicemia_por_id(
+    uow: AbstractUnitOfWork,
+    usuario_id: UUID,
+    glicemia_id: UUID,
+) -> Glicemia:
+
+    with uow:
+        print(usuario_id)  # TODO: vincular glicemias a usuarios ou outra entidade
+        glicemia = uow.repo.consultar_por_id(id=glicemia_id)
+
+    return glicemia

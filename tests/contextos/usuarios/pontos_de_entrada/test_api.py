885abf6e0b77590eba1707be5076f4f5098dbe0d
Thiago Brasil <tchaguitos@gmail.com>
2022-03-31 20:07:01 -0300

    test(usuarios/pontos_de_entrada): add teste para endpoint de alterar email
    adiciona testes para endpoint para alterar email de usuario

--- tests/contextos/usuarios/pontos_de_entrada/test_api.py
+++ tests/contextos/usuarios/pontos_de_entrada/test_api.py
@@ -64,6 +64,46 @@ def test_editar_usuario(session):
 
 
 @freeze_time(datetime(2021, 8, 27, 16, 20))
+def test_alterar_email_do_usuario(session):
+    data_de_nascimento = date(1995, 8, 27)
+
+    usuario_a_ser_criado = {
+        "email": "nome-teste@teste.com.br",
+        "senha": "senha123",
+        "nome_completo": "Nome completo",
+        "data_de_nascimento": str(data_de_nascimento),
+    }
+
+    response = client.post(
+        "/v1/usuarios",
+        json=usuario_a_ser_criado,
+    )
+    usuario_id = response.json().get("id")
+
+    response = client.get(f"/v1/usuarios/{usuario_id}")
+
+    email_do_usuario = response.json().get("email")
+
+    assert email_do_usuario == "nome-teste@teste.com.br"
+
+    response = client.patch(
+        f"/v1/usuarios/{usuario_id}/alterar-email",
+        json={
+            "novo_email": "tchaguitos@gmail.com",
+        },
+    )
+
+    assert "id" in response.json()
+    assert response.status_code == 200
+
+    response = client.get(f"/v1/usuarios/{usuario_id}")
+
+    email_do_usuario = response.json().get("email")
+
+    assert email_do_usuario == "tchaguitos@gmail.com"
+
+
+@freeze_time(datetime(2021, 8, 27, 16, 20))
 def test_consultar_usuarios_por_id(session):
     data_de_nascimento = date(1995, 8, 27)
 

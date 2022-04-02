e8473754df973ce1fb996aac675cb419e42fe6f6
Thiago Brasil <tchaguitos@gmail.com>
2022-04-02 19:56:01 -0300

    feat(usuarios/dominio): add eventos
    adiciona arquivo para eventos

---
+++ contextos/usuarios/dominio/eventos.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+from dataclasses import dataclass
+from dataclass_type_validator import dataclass_validate
+
+from libs.ddd import Evento
+
+
+@dataclass_validate
+@dataclass
+class EmailAlterado(Evento):
+    usuario_id: UUID

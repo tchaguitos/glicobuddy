5398fc9be80e31885884b3d6e98acde39bf33199
Thiago Brasil <tchaguitos@gmail.com>
2022-04-04 20:13:56 -0300

    refactor(usuarios/repositorio): remove metodos de consulta do repo dominio
    remove metodos de consulta do repo dominio, mantendo apenas os metodos para persistência de dados

--- contextos/usuarios/repositorio/repo_dominio.py
+++ contextos/usuarios/repositorio/repo_dominio.py
@@ -1,35 +1,9 @@
-from uuid import UUID
-from typing import Iterator
+from libs.repositorio import RepositorioDominio
 
-from libs.repositorio import SqlAlchemyRepository
-
-from contextos.usuarios.dominio.entidades import (
-    Email,
-    Usuario,
-)
+from contextos.usuarios.dominio.entidades import Usuario
 
 
-class RepoAbstratoUsuarios(SqlAlchemyRepository):
-    def adicionar(self, usuario: Usuario):
-        raise NotImplementedError
-
-    def remover(self, usuario: Usuario):
-        raise NotImplementedError
-
-    def consultar_todos(self):
-        raise NotImplementedError
-
-    def consultar_por_id(self, id: UUID) -> Usuario:
-        raise NotImplementedError
-
-    def consultar_por_email(self, email: Email) -> Usuario:
-        raise NotImplementedError
-
-
-class RepoDominioUsuarios(RepoAbstratoUsuarios):
-    def __init__(self, session):
-        self.session = session
-
+class RepoDominioUsuarios(RepositorioDominio):
     def adicionar(self, usuario: Usuario):
         self.session.add(usuario)
 
@@ -38,12 +12,3 @@ class RepoDominioUsuarios(RepoAbstratoUsuarios):
 
     def remover(self, usuario: Usuario):
         self.session.delete(usuario)
-
-    def consultar_todos(self) -> Iterator[Usuario]:
-        return self.session.query(Usuario).all()
-
-    def consultar_por_id(self, id: UUID) -> Usuario:
-        return self.session.query(Usuario).filter_by(id=id).first()
-
-    def consultar_por_email(self, email: Email) -> Usuario:
-        return self.session.query(Usuario).filter_by(email=email).first()

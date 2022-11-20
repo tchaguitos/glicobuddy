5f1433b0fc5704830e1f4722c099a81b0a7cbc6f
Thiago Brasil <tchaguitos@gmail.com>
2022-11-20 18:31:08 -0300

    refactor(glicemias/pontos_de_entrada): utiliza message bus para tratar mensagens

--- tests/contextos/glicemias/pontos_de_entrada/test_api.py
+++ tests/contextos/glicemias/pontos_de_entrada/test_api.py
@@ -151,20 +151,22 @@ def test_editar_glicemia(session):
     assert "id" in response.json()
     assert response.status_code == 200
 
-    with pytest.raises(Glicemia.ValorDeGlicemiaInvalido) as e:
-        valores_para_edicao_de_glicemia = {
-            "valor": 13,
-            "observacoes": "nao vai dar certo",
-            "primeira_do_dia": False,
-            "horario_dosagem": str(horario_dosagem),
-        }
+    valores_para_edicao_de_glicemia = {
+        "valor": 13,
+        "observacoes": "nao vai dar certo",
+        "primeira_do_dia": False,
+        "horario_dosagem": str(horario_dosagem),
+    }
 
-        response = client.patch(
-            f"/v1/glicemias/{glicemia_id}",
-            json=valores_para_edicao_de_glicemia,
-        )
+    response = client.patch(
+        f"/v1/glicemias/{glicemia_id}",
+        json=valores_para_edicao_de_glicemia,
+    )
 
-        assert str(e.value) == "O valor da glicemia deve ser superior a 20mg/dl"
+    assert (
+        response.json().get("detail")
+        == "O valor da glicemia deve ser superior a 20mg/dl"
+    )
 
 
 @freeze_time(datetime(2021, 8, 27, 16, 20))

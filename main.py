9d8b6b70f4a59f7a68d72c7d377e366482a5bdd7
Thiago Brasil <tchaguitos@gmail.com>
2022-11-27 20:50:31 -0300

    feat(main): add crypt context

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from passlib.context import CryptContext
+
 from contextos.usuarios.pontos_de_entrada.api import router as router_usuarios
 from contextos.glicemias.pontos_de_entrada.api import router as router_glicemias
+
+auth_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
 

311d47a43c3a0c96f64c7cc91ccc5cbee49cdbe1
Thiago Brasil <tchaguitos@gmail.com>
2022-12-26 20:45:15 -0300

    feat: add migrations para alterações em tabelas do banco de dados (#33)
    * deps(requirements): adiciona alembic ao projeto
    
    * feat(migrations): add migration para criar tabela de usuarios
    
    * test(usuarios): melhora mock de usuario salvo
    
    * infra(docker-compose): add banco de testes
    
    * ci(actions): utiliza variavel de ambiente em docker-compose e gituhub actions

---
+++ migrations/versions/7bb2d0bae7da_criar_tabela_de_usuarios.py
@@ -0,0 +1,48 @@
+"""criar_tabela_de_usuarios
+
+Revision ID: 7bb2d0bae7da
+Revises: 
+Create Date: 2022-12-26 18:58:22.889879
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+from sqlalchemy.dialects.postgresql import UUID
+
+revision = "7bb2d0bae7da"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "usuario",
+        sa.Column("id", UUID(as_uuid=True), primary_key=True, unique=True),
+        sa.Column("email", sa.String(155)),
+        sa.Column("senha", sa.String(255)),
+        sa.Column("nome_completo", sa.String(255)),
+        sa.Column("data_de_nascimento", sa.Date()),
+        sa.Column(
+            "data_criacao_utc",
+            sa.DateTime(),
+            server_default=sa.func.current_timestamp(),
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    op.create_unique_constraint(
+        columns=["email"],
+        table_name="usuario",
+        constraint_name="constraint_usuario_email",
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("usuario")
+    op.drop_constraint(
+        type_="unique",
+        table_name="usuario",
+        constraint_name="constraint_usuario_email",
+    )

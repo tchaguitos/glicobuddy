807b9fd80f0133cfe3faa9a42fa926c21efd67bb
Thiago Brasil <tchaguitos@gmail.com>
2022-12-26 20:47:33 -0300

    feat(libs/tipos_basicos): add classe para valor de glicemia

---
+++ libs/tipos_basicos/numeros.py
@@ -0,0 +1,19 @@
+class ValorDeGlicemiaInvalido(Exception):
+    pass
+
+
+class ValorDeGlicemia(int):
+    """"""
+
+    def __new__(cls, valor: str):
+        if not isinstance(valor, int):
+            raise ValorDeGlicemiaInvalido(
+                "Você deve fornecer um valor de glicemia válido"
+            )
+
+        if not valor > 20:
+            raise ValorDeGlicemiaInvalido(
+                "O valor da glicemia deve ser superior a 20mg/dl"
+            )
+
+        return super().__new__(cls, valor)

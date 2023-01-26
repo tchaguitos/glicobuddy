81f9670a6a8d4b2652c747cc9fd96a2fbd75f7d5
Thiago Brasil <tchaguitos@gmail.com>
2023-01-25 21:54:02 -0300

    fix(libs/pontos_de_entrada): corrige excecao tratada ao validar token

--- libs/pontos_de_entrada.py
+++ libs/pontos_de_entrada.py
@@ -1,12 +1,10 @@
-from jose import JWTError
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from libs.unidade_de_trabalho import UnidadeDeTrabalho
 from libs.tipos_basicos.identificadores_db import IdUsuario
 
-from contextos.usuarios.adaptadores.jwt import GeradorDeToken
+from contextos.usuarios.adaptadores.jwt import GeradorDeToken, TokenExpirado
 from contextos.usuarios.servicos.visualizadores import consultar_usuario_por_id
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/usuarios/login")
@@ -15,8 +13,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/usuarios/login")
 def retornar_usuario_logado(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Não foi possível validar suas credenciais. Faça login novamente",
         headers={"WWW-Authenticate": "Bearer"},
+        detail="Não foi possível validar suas credenciais. Faça login novamente",
     )
 
     try:
@@ -26,7 +24,7 @@ def retornar_usuario_logado(token: str = Depends(oauth2_scheme)):
         if usuario_id is None:
             raise credentials_exception
 
-    except JWTError:
+    except TokenExpirado:
         raise credentials_exception
 
     usuario_logado = consultar_usuario_por_id(
